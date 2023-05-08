73fc088f4fa65c14011e15c162e2a42d49782204
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-05-07 20:58:31 -0700

    Train with almost all annotations, use NER

--- sp-week-6/irr-for-model-eval.py
+++ sp-week-6/irr-for-model-eval.py
@@ -13,40 +13,25 @@ df = pd.read_json(
 # Load the model
 nlp = spacy.load("./model/model-best")
 
-print("Evaluating IRR data for spans...")
+print("Evaluating IRR data for entities...")
 print("=" * 80)
 
-# Iter each example and check for spans
+# Iter each example and check for ents
 processed = []
 for _, row in df.iterrows():
     # Process
     doc = nlp(row.text)
 
+    # Append results to list
+    print("Original text:")
+    print(f"\t{row.text}")
+    print()
+
     # Check for spans
-    for span_group in doc.spans:
-        longest_span_for_pawo = ""
-        longest_span_for_p = ""
-        group = doc.spans[span_group]
-        for span in group:
-            if (
-                span.label_ == "PERSON" 
-                and len(span.text) > len(longest_span_for_p)
-            ):
-                longest_span_for_p = span.text
-            elif (
-                span.label_ == "PERSON-AFFLIATED-WITH-ORG" 
-                and len(span.text) > len(longest_span_for_pawo)
-            ):
-                longest_span_for_pawo = span.text
-           
-        # Append results to list
-        print("Original text:")
-        print(f"\t{row.text}")
-        print()
-        print("PERSON-AFFLIATED-WITH-ORG:")
-        print(f"\t{longest_span_for_pawo}")
-        print()
-        print("PERSON:")
-        print(f"\t{longest_span_for_p}")
-        print()
-        print("-" * 40)
+    for ent in doc.ents:
+        print(ent.label_)
+        print(ent.text)
+    
+    print("-" * 80)
+    print()
+    print()
